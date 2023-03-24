de0e0cea58446a69304e18d0b3464acf08441778
cgasenjo <sr.asenjo@hotmail.com>
2023-03-23 18:15:28 -0600

    Main prueba MAPS

--- src/components/GoogleMapsPam.js
+++ src/components/GoogleMapsPam.js
@@ -1,13 +1,15 @@
-import React from "react";
-import { GoogleMap, useJsApiLoader, LoadScript, Marker} from '@react-google-maps/api';
-
+import React, { useEffect, useState } from "react";
+import { GoogleMap, useJsApiLoader, LoadScript, InfoWindow, Marker } from '@react-google-maps/api';
 
 const GoogleMapsPam = () => {
 
-  const mapStyles = {        
+  const [markerPosition, setMarkerPosition] = useState(null);
+
+  const mapStyles = {
     height: "100vh",
-    width: "100%"};
-  
+    width: "100%"
+  };
+
   const defaultCenter = {
     lat: 10, lng: -84
   }
@@ -15,46 +17,50 @@ const GoogleMapsPam = () => {
   const locations = [
     {
       name: "Location 1",
-      location: { 
+      location: {
         lat: 10.2,
-        lng: -83.5 
+        lng: -83.5
       },
     },
     {
       name: "Location 2",
-      location: { 
+      location: {
         lat: 10.5,
         lng: -83
 
       },
     },
-    
- 
-    
   ];
+
+  function handleMapClick(event) {
+    setMarkerPosition({
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng()
+    });
+  }
+
+
   return (
     <div>
-     
 
-     <GoogleMap
-          mapContainerStyle={mapStyles}
-          zoom={10}
-          center={defaultCenter}>
-         { 
-         
-            locations.map(item => {
-              return (
-              <Marker key={item.name} position={item.location}/>
-              )
-            })
-         
-         }
-         
-     </GoogleMap>
+
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={10}
+        center={defaultCenter}
+        onClick={({ lat, lng }) => setMarkerPosition({ lat, lng })}
+      >
+        {markerPosition && (
+          <Marker position={markerPosition} />
+        )}
+        
+      </GoogleMap>
 
     </div>
   );
 };
+
+
 
 export default GoogleMapsPam;
 
