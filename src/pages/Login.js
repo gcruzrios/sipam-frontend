980b9a988d6dd3733b22011a46db18f655bfc8f3
cgasenjo85 <cristian.gutierrez@geoinn.net>
2023-05-04 19:45:55 -0600

    sweetAlert implementado en User/pass incorrecto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,199 +9,168 @@ import { Link } from "react-router-dom";
 export const Login = () => {
 
     const [usuario, setUsuario] = useState('');
-    const [password, setPassword] = useState('')
-  
+    const [password, setPassword] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+
 
     useEffect(() => {
         obtenerToken();
-      
+    }, []);
 
-    }, [])
-    
+    const obtenerToken = async () => {
 
-    const obtenerToken= async() => {
-        
-        const auth_user = {Username:"c0n4p4n$AppSIPAMUser", Password:"c0n4p4n$AppSIPAMpass"}
-        const respuesta= await axios.post(`/wsSIPAM/Authenticate`, auth_user);
+        const auth_user = { Username: "c0n4p4n$AppSIPAMUser", Password: "c0n4p4n$AppSIPAMpass" }
+        const respuesta = await axios.post(`/wsSIPAM/Authenticate`, auth_user);
 
         const token = respuesta.data.Token;
-                  
+
         localStorage.setItem('Token', token);
 
     }
 
-    const login= async(e) =>{
+    const handleLogin = async (e) => {
         e.preventDefault();
-        
-        const ingreso = {usuario, password}
-        
+
+        const ingreso = { usuario, password }
         const Token = localStorage.getItem('Token');
 
+        console.log(Token);
 
+        const response = await axios.post(`/wsSIPAM/GetUsuario`, ingreso, { headers: { Authorization: 'Bearer ' + Token } }, false)
 
-        console.log(ingreso);
-
-          
-        const response= await axios.post(`/wsSIPAM/GetUsuario`,ingreso,{headers:{Authorization:'Bearer '+Token}})
-        //console.log(response);
-
-      
         const mensaje = response.data.CodigoResultado;
-        const mensaje_alerta= response.data.MensajeResultado;
-        console.log(mensaje);
-        console.log(mensaje_alerta);
+        const mensaje_alerta = response.data.MensajeResultado;
 
-                 
-        
-
-      
-        if(mensaje !=='200'){
-                     
+        if (mensaje !== '200') {
             Swal.fire({
-               
-                icon: 'error',
-                title: mensaje_alerta,
-                showConfirmButton: false,
-                timer: 1500
+                text: 'Usuario o contraseña incorrectas..',
+                icon: 'error'
             })
-
-        }else{
-
+        }
+        else {
             const idUsuario = response.data.Resultado[0].idUsuario;
             const nombreUsuario = response.data.Resultado[0].nombreCompleto;
-            const Usuario = response.data.Resultado[0].usuario;
-            const correoUsuario = response.data.Resultado[0].correo;
-            
             const rolUsuario = response.data.Resultado[0].nombreRol;
-            const idOrganizacion = response.data.Resultado[0].idOrganizacion;
-            const organizacion = response.data.Resultado[0].organizacion;
-           
-            const estadoUsuario = response.data.Resultado[0].estado;
 
             const estado = 'activo';
             localStorage.setItem('Estado', estado);
-            localStorage.setItem('idUsuario',idUsuario );
-            localStorage.setItem('Usuario',Usuario );
-            localStorage.setItem('correoUsuario',correoUsuario );
+            localStorage.setItem('idUsuario', idUsuario);
+            localStorage.setItem('nombreUsuario', nombreUsuario);
+            localStorage.setItem('rolUsuario', rolUsuario);
 
-            localStorage.setItem('nombreUsuario',nombreUsuario );
-            localStorage.setItem('estadoUsuario',estadoUsuario );
-            localStorage.setItem('rolUsuario',rolUsuario );
-            localStorage.setItem('idOrganizacion',idOrganizacion );
-            localStorage.setItem('organizacion',organizacion );
-            
-            window.location.href='/index'
-           
+            window.location.href = '/index'
+
+
         }
 
     }
-   
-    
 
-  return (
-    <>
+    return (
+        <>
 
-        <main className="main-content">
+            <main className="main-content">
 
-            <div className="admin">
-                <div className="container-fluid p-1">
-                    <div className="row justify-content-center">
-                
-                    <div className="col-xxl-3 col-xl-4 col-md-6 col-sm-8 ">
-                        <div className="edit-profile">
-                            <div className="edit-profile__logos">
-                                <a href="index.html">
-                                <img className="dark" src="/img/logo-white.png" alt="" />
-                                <img className="light" src="/img/logo-white.png" alt=""/>
-                                </a>
-                            </div>
-                            <div className="card border-0">
-                                <div className="card-header">
-                                <div className="edit-profile__title">
-                                    <h6>Entrar al SIPAM</h6>
-                                </div>
-                                </div>
-                                <div className="card-body">
-                                <form onSubmit={ login }>
-                                    <div className="edit-profile__body">
-                                        <div className="form-group mb-25">
-                                            <label htmlFor="username">Email</label>
-                                            <input type="text" className="form-control" id="username" placeholder="ufo@conapam.go.cr" required onChange={(e)=>setUsuario(e.target.value)}/>
+                <div className="admin">
+                    <div className="container-fluid p-1">
+                        <div className="row justify-content-center">
+
+                            <div className="col-xxl-3 col-xl-4 col-md-6 col-sm-8 ">
+                                <div className="edit-profile">
+                                    <div className="edit-profile__logos">
+                                        <a href="index.html">
+                                            <img className="dark" src="/img/logo-white.png" alt="" />
+                                            <img className="light" src="/img/logo-white.png" alt="" />
+                                        </a>
+                                    </div>
+                                    <div className="card border-0">
+                                        <div className="card-header">
+                                            <div className="edit-profile__title">
+                                                <h6>Entrar al SIPAM</h6>
+                                            </div>
                                         </div>
-                                        <div className="form-group mb-15">
-                                            <label htmlFor="password-field">contraseña</label>-
-                                            <div className="position-relative">
-                                                <input id="password-field" type="password" className="form-control" name="password" placeholder="Password" required onChange={(e)=>setPassword(e.target.value)} />
-                                                <div className="uil uil-eye-slash text-lighten fs-15 field-icon toggle-password2">
+                                        <div className="card-body">
+                                            <form>
+                                                <div className="edit-profile__body">
+                                                    <div className="form-group mb-25">
+                                                        <label htmlFor="username">Email</label>
+                                                        <input type="text" className="form-control" id="username" placeholder="ufo@conapam.go.cr" required onChange={(e) => setUsuario(e.target.value)} />
+                                                    </div>
+                                                    <div className="form-group mb-15">
+                                                        <label htmlFor="password-field">contraseña</label>-
+                                                        <div className="position-relative">
+                                                            <input id="password-field" type="password" className="form-control" name="password" placeholder="Password" required onChange={(e) => setPassword(e.target.value)} />
+                                                            <div className="uil uil-eye-slash text-lighten fs-15 field-icon toggle-password2">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div className="admin-condition">
+                                                        <div className="checkbox-theme-default custom-checkbox ">
+                                                            <input className="checkbox" type="checkbox" id="check-1" />
+                                                            <label htmlFor="check-1">
+                                                                <span className="checkbox-text">Mantenerme conectado</span>
+                                                            </label>
+                                                        </div>
+                                                        <Link className="menu-text" to={`/recordarpassword`}>  ¿Olvido la contraseña?</Link>
+
+                                                    </div>
+
+                                                    <div className="admin__button-group button-group d-flex pt-1 justify-content-md-start justify-content-center">
+                                                        <button
+                                                            onClick={handleLogin}
+                                                            className="btn btn-primary btn-default w-100 btn-squared text-capitalize lh-normal px-50 signIn-createBtn ">
+                                                            Entrar
+                                                        </button>
+                                                    </div>
+
+
                                                 </div>
-                                            </div>
+                                            </form>
                                         </div>
-                                        <div className="admin-condition">
-                                            <div className="checkbox-theme-default custom-checkbox ">
-                                                <input className="checkbox" type="checkbox" id="check-1" />
-                                                <label htmlFor="check-1">
-                                                <span className="checkbox-text">Mantenerme conectado</span>
-                                                </label>
-                                            </div>
-                                            <Link  className="menu-text" to={`/recordarpassword`}>  ¿Olvido la contraseña?</Link>
-                                           
-                                        </div>
-                                
-                                        <div className="admin__button-group button-group d-flex pt-1 justify-content-md-start justify-content-center">
-                                            <button className="btn btn-primary btn-default w-100 btn-squared text-capitalize lh-normal px-50 signIn-createBtn ">
-                                            Entrar
-                                            </button>
-                                        </div>
-                                   
-                                   
-                                </div>
-                                </form>
-                                </div>
-                            
-                                <div className="admin-topbar">
-                                {/* <p className="mb-0">
+
+                                        <div className="admin-topbar">
+                                            {/* <p className="mb-0">
                                     No tiene cuenta aún?
                                     
                                     <Link  className="menu-text" to={`/registro`}>  Registrarse</Link>
                                     
                                 </p> */}
-                                
+
+                                        </div>
+
+                                    </div>
                                 </div>
-                                
+
                             </div>
+
                         </div>
-                            
                     </div>
-                        
+                </div>
+                <Footer />
+
+            </main>
+            <div id="overlayer">
+                <div className="loader-overlay">
+                    <div className="dm-spin-dots spin-lg">
+                        <span className="spin-dot badge-dot dot-primary"></span>
+                        <span className="spin-dot badge-dot dot-primary"></span>
+                        <span className="spin-dot badge-dot dot-primary"></span>
+                        <span className="spin-dot badge-dot dot-primary"></span>
                     </div>
                 </div>
             </div>
-           <Footer/>
-
-        </main>
-        <div id="overlayer">
-            <div className="loader-overlay">
-                <div className="dm-spin-dots spin-lg">
-                    <span className="spin-dot badge-dot dot-primary"></span>
-                    <span className="spin-dot badge-dot dot-primary"></span>
-                    <span className="spin-dot badge-dot dot-primary"></span>
-                    <span className="spin-dot badge-dot dot-primary"></span>
-                </div>
+            <div className="enable-dark-mode dark-trigger">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i className="uil uil-moon"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
-        </div>
-        <div className="enable-dark-mode dark-trigger">
-            <ul>
-                <li>
-                    <a href="#">
-                    <i className="uil uil-moon"></i>
-                    </a>
-                </li>
-            </ul>
-        </div>
 
 
-    </>
-  )
+        </>
+    )
 }
 
 
